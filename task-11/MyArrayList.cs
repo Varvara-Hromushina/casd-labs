54c077550754aa3989c0a3a0c003c27fc49794e8
Varvara-Hromushina <varvara.hromushina@yandex.ru>
2024-11-05 14:36:44 +0300

    Add files via upload

---
+++ task-11/MyArrayList.cs
@@ -0,0 +1,261 @@
+﻿using System.ComponentModel;
+using System.Drawing;
+using System.Reflection.Metadata;
+
+
+class MyArrayList<T>
+{
+    private T[] elementData; 
+    private int size; 
+
+    public MyArrayList() {size = 0;}
+
+    public MyArrayList(T[] arr) 
+    {
+        elementData = new T[arr.Length];
+        size = arr.Length;
+        for(int i = 0; i < size; i++)
+        {
+            elementData[i] = arr[i];
+        }
+    }
+
+    public MyArrayList(int capacity)
+    {
+        elementData = new T[capacity];
+        size = 0;
+    }
+
+    public T Value(int index)
+    {
+        return elementData[index];
+    }
+
+    public void Add(T element)
+    {
+        if(size == elementData.Length) Resize();
+        elementData[size] = element;
+        size++;
+    }
+
+    public void Resize()
+    {
+        int newSize = (int)(elementData.Length * 1.5) + 1;
+            T[] newElementData = new T[newSize];
+            for(int i = 0; i < size; i++)
+            {
+                newElementData[i] = elementData[i];
+            }
+            elementData = newElementData;
+    }
+    
+    public void Add(T[] arr)
+    {
+        foreach(T element in arr) Add(element);
+    }
+
+    public void Clear()
+    {
+        elementData = null;
+        size = 0;
+    }
+
+    public bool Contains(object obj)
+    {
+        foreach(T element in elementData)
+        {
+            if(element.Equals(obj)) return true;
+        }
+        return false;
+    }
+
+    public bool ContainsAll(T[] arr)
+    {
+        bool flag = true;
+        foreach(T element in arr)
+        {
+            if(flag)
+            {
+                flag = false;
+                foreach(T elems in elementData)
+                {
+                    if(element.Equals(elems))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            else break;
+        }
+        return flag;
+    }
+
+    public bool IsEmpty()
+    {
+        if(size == 0) return true;
+        else return false;
+    }
+
+    public void Remove(object obj)
+    {
+        for(int i = 0; i < elementData.Length; i++)
+        {
+            if(elementData[i].Equals(obj))
+            {
+                for(int j = i; j < elementData.Length - 1; j++)
+                {
+                    elementData[j] = elementData[j + 1];
+                }
+                size--;
+                Array.Resize(ref elementData, size);
+                return;
+            }
+        }   
+    }
+
+    public void RemoveAll(T[] arr)
+    {
+        foreach(T element in arr) Remove(element);
+    }
+
+    public void RetainAll(T[] arr)
+    {
+        
+        T[] arrCopy = new T[elementData.Length];
+        for (int i = 0; i < elementData.Length; i++) arrCopy[i] = elementData[i];
+        if(ContainsAll(arr) != true) 
+        {
+            Console.WriteLine("Некоторые элементы отсутствуют в массиве");
+            return;
+        }
+        bool flag = true;
+        foreach(T element in arrCopy)
+        {
+            foreach(T elems in arr)
+            {
+                if (element.Equals(elems)) 
+                {   
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag == true) Remove(element);
+            flag = true;
+        }
+        Array.Clear(arrCopy);
+    }
+
+    public int Size()
+    {
+        return size;
+    }
+
+    public T[] ToArray()
+    {
+        T[] newArray = new T[size];
+        for(int i = 0; i < size; i++)
+        {
+            newArray[i] = elementData[i];
+        }
+        return newArray;
+    }
+
+    public T[] ToArray(T[]? arr)
+    {
+
+        if (arr!= null && arr.Length < size) 
+        {
+            Console.WriteLine("В массиве недостаточно места для хранения динамического массива");
+            return arr;
+        }
+
+        else if(arr == null) arr = new T[size];
+        for(int i = 0; i < arr.Length; i++) arr[i] = elementData[i];
+        return arr;
+    }
+
+    public void Add(int index, T element)
+    {
+        if ((index < 0) || (index > size)) throw new ArgumentOutOfRangeException("index");
+        if(size == elementData.Length) Resize();
+        for(int j = size; j > index; j--) elementData[j] = elementData[j - 1];
+        elementData[index] = element;
+        size++;
+    }
+
+    public void AddAll(int index, T[] arr)
+    {
+        T[] newArray = new T[size + arr.Length];
+        for(int i = 0; i < index; i++)
+        {
+            newArray[i] = elementData[i];
+        }
+        int j = 0;
+        for(int i = index; i < index + arr.Length; i++)
+        {
+            newArray[i] = arr[j]; j++;
+        }
+        int k = index;
+        for(int i = index + arr.Length; i < size + arr.Length; i++)
+        {
+            newArray[i] = elementData[k]; k++;
+        }
+        elementData = newArray;
+        size = newArray.Length;
+    }
+
+    public T Get(int index)
+    {
+        if ((index < 0) || (index > size)) throw new ArgumentOutOfRangeException("index");
+        else return elementData[index];
+    }
+
+    public int IndexOf(object obj)
+    {
+        for(int i = 0; i < size; i++) if(obj.Equals(elementData[i])) return i;
+        return -1;
+    }
+
+    public int LastIndexOf(object obj)
+    {
+        for(int i = size - 1; i > 0; i--) if(obj.Equals(elementData[i])) return i;
+        return -1;
+    }
+
+    public T Remove(int index)
+    {
+        if ((index < 0) || (index > size)) throw new ArgumentOutOfRangeException("index");
+        T element = elementData[index];
+        Remove(element);
+        return element;
+    }
+
+    public void Set(int index, T element)
+    {
+        if ((index < 0) || (index >= size)) throw new ArgumentOutOfRangeException("index");
+        elementData[index] = element;
+        int newCapacity = (int)(elementData.Length * 1.5) + 1;
+        size= newCapacity;
+    }
+
+    public T[] SubList(int fromIndex, int toIndex)
+    {   
+        if ((fromIndex < 0) || (fromIndex > size)) throw new ArgumentOutOfRangeException("fromIndex");
+        if ((toIndex < 0) || (toIndex > size)) throw new ArgumentOutOfRangeException("toIndex");
+        int k = 0;
+        T[] newArray = new T[toIndex - fromIndex];
+        for(int i = fromIndex; i < toIndex; i++)
+        {
+            newArray[k] = elementData[i]; k++;
+        }
+        return newArray;
+    }
+
+    public void Print()
+    {
+        if(size !=0) {for(int i = 0; i < size; i++) Console.Write(elementData[i] + " ");}
+        Console.WriteLine();
+    }
+}
+
---
+++ task-11/MyHeap.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+
+public class MyHeap<T> where T: IComparable<T>
+{
+    private MyArrayList<T> heap = new MyArrayList<T>(10);
+    private Comparer<T> comparer;
+
+    private int size;
+    
+    public MyHeap(T[] array)
+    {
+        size = array.Length;
+        for (int i = 0; i < size; i++) heap.Add(array[i]);
+
+        comparer = Comparer<T>.Default;    
+        for (int i = size / 2 - 1; i >= 0; i--) Heapify(i);
+    }
+
+    public Comparer<T> ComparatorGet()
+    {
+        return comparer;
+    }
+
+    public void ComparatorSet(Comparer<T> comparator)
+    {
+        this.comparer = comparator;
+    }
+    
+    public T SearchMaximum() => heap.Get(0);
+    public T DeleteMaximum()
+    {
+        T maximum = heap.Get(0);
+        heap.Remove(0);
+        size--;
+        Heapify(0);
+        return maximum;
+    }
+
+  // восстанавливаем порядок
+    public void Heapify(int i)
+    {
+        int left;
+        int right;
+        int parents = i;
+        while (true)
+        {
+            left = 2 * i + 1;
+            right  = 2 * i + 2;
+
+            if(right < size && comparer.Compare(heap.Get(right),heap.Get(parents)) > 0)
+                parents = right;
+
+            if (left < size && comparer.Compare(heap.Get(left),heap.Get(parents)) > 0)
+                parents = left;
+
+            if (parents == i)
+                break;
+
+            Swap(parents, i);
+            i = parents;
+        }
+    }
+
+    
+    private void Swap(int i, int j) 
+    {
+        T temp1 = heap.Get(i);
+        T temp2 = heap.Get(j);
+        heap.Set(j, temp1);
+        heap.Set(i, temp2);
+    }
+    
+
+    public void KeyIncr(int index, T e)
+    {
+        if (index > heap.Size() - 1) throw new IndexOutOfRangeException();
+
+        heap.Set(index, e);
+        for (int i = size / 2; i >= 0; i--) Heapify(i);
+    }
+
+    public void AddElement(T element)
+    {
+        heap.Set(size, element);
+        size++;
+        for (int i = size / 2; i >= 0; i--) Heapify(i);
+    }
+
+    public T Get(int index)
+    {
+        return heap.Get(index);
+    }
+
+    public void HeapMerge(MyHeap<T> newHeap)
+    {
+        while(newHeap.size > 0)
+        {
+            T element = newHeap.DeleteMaximum();
+            AddElement(element);
+        }
+        for(int i = size / 2; i >= 0; i--)
+            Heapify(i);
+    }
+
+    public void Print()
+    {
+        for (int i = 0; i < size; i++) Console.Write(heap.Get(i) + " ");
+        Console.WriteLine();
+    }
+}
