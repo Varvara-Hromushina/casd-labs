c9377c865bf9528eaf03d8f0d27470ec761c21c2
Varvara-Hromushina <varvara.hromushina@yandex.ru>
2024-11-29 19:08:13 +0300

    Add files via upload

---
+++ task-17/task-17/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace task_17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(197, 48);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(591, 368);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Add (element)",
+            "Add (index, element)",
+            "Get (element)",
+            "Set (element)",
+            "Remove (element)"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 129);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(149, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(51, 185);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "График зависимости";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ task-17/task-17/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using ZedGraph;
+
+namespace task_17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGraph();
+        }
+
+
+        private void InitializeGraph()
+        {
+            zedGraphControl1.GraphPane.Title.Text = "Зависимость времени от количества элементов в массиве";
+            zedGraphControl1.GraphPane.XAxis.Title.Text = "Размер массива, шт";
+            zedGraphControl1.GraphPane.YAxis.Title.Text = "Время выполнения, мс";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            PointPairList list1 = new PointPairList();
+            PointPairList list2 = new PointPairList();
+            GraphPane pane = zedGraphControl1.GraphPane; 
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    MyArrayList<int> list = new MyArrayList<int>(10);
+                    MyLinkedList<int> linkedlist = new MyLinkedList<int>();
+                    int size;
+                    for (size = 100; size <= 100000; size *= 10)
+                    {
+                        double sum = 0;
+                        double sum1 = 0;
+                        for (int j = 0; j < 20; j++)
+                        {
+                            Stopwatch timer = new Stopwatch();
+                            timer.Start();
+                            for (int i = 0; i < size; i++) list.Add(i);
+                            timer.Stop();
+                            sum += timer.ElapsedMilliseconds;
+
+                            Stopwatch timer1= new Stopwatch();
+                            timer1.Start();
+                            for (int i = 0; i < size; i++) linkedlist.Add(i);
+                            timer1.Stop();
+                            sum1 += timer1.ElapsedMilliseconds;
+                        }
+                        double rez = sum / 20; // 20 операций => делим на 20
+                        double rez2= sum1 / 20;
+                        list1.Add(size, rez);
+                        list2.Add(size, rez2);
+                        list.Clear();
+                        linkedlist.Clear();
+                    }
+                    break;
+                case 1:
+                    list = new MyArrayList<int>(10);
+                    linkedlist = new MyLinkedList<int>();
+                    for (size = 100; size <= 1000; size *= 10)
+                    {
+                        double sum = 0;
+                        double sum1 = 0;
+                        for (int j = 0; j < 20; j++)
+                        {
+                            for (int i = 0; i < size; i++) list.Add(i);
+                            for (int i = 0; i < size; i++) linkedlist.Add(i);
+
+                            Random rand = new Random();
+                            int w = rand.Next(0, size - 1);
+
+                            Stopwatch stopwatch = new Stopwatch();
+                            stopwatch.Start();
+                            for (int i = 0; i < size; i++)
+                            {
+                                int index = rand.Next(0, list.Size() - 1);
+                                int number = rand.Next(0, 100000); list.Add(index, number);
+                            }
+                            stopwatch.Stop();
+                            sum += stopwatch.ElapsedMilliseconds;
+
+                            Stopwatch stopwatch1 = new Stopwatch();
+                            stopwatch1.Start();
+                            for (int i = 0; i < size; i++)
+                            {
+                                int index = rand.Next(0, linkedlist.Size() - 1);
+                                int number = rand.Next(0, 100000); linkedlist.Set(index, number);
+                            }
+                            stopwatch1.Stop();
+                            sum1 += stopwatch1.ElapsedMilliseconds;
+                        }
+                        double rez = sum / 20;
+                        double rez2 = sum1 / 20;
+                        list1.Add(size, rez);
+                        list2.Add(size, rez2);
+                        list.Clear();
+                        linkedlist.Clear();
+                    }
+                    break;
+                case 2:
+                    list = new MyArrayList<int>(10);
+                    linkedlist = new MyLinkedList<int>();
+                    for (size = 100; size <= 1000; size *= 10)
+                    {
+                        double sum = 0;
+                        double sum1 = 0;
+                        for (int j = 0; j < 20; j++)
+                        {
+                            for (int i = 0; i < size; i++) list.Add(i);
+                            for (int i = 0; i < size; i++) linkedlist.Add(i);
+
+                            Random rand = new Random();
+                            int w=rand.Next(0,size-1);
+
+                            Stopwatch stopwatch = new Stopwatch();
+                            stopwatch.Start();
+                            for(int i=0;i<size;i++) list.Get(w);
+                            stopwatch.Stop();
+                            sum += stopwatch.ElapsedMilliseconds;
+
+                            Stopwatch stopwatch1 = new Stopwatch();
+                            stopwatch1.Start();
+                            for(int i=0;i<size;i++) linkedlist.Get(w);
+                            stopwatch1.Stop();
+                            sum1 += stopwatch1.ElapsedMilliseconds;
+                        }
+                        double rez = sum / 20;
+                        double rez2 = sum1 / 20;
+                        list1.Add(size, rez);
+                        list2.Add(size, rez2);
+                        list.Clear();
+                        linkedlist.Clear();
+                    }
+                        break;
+                    
+                case 3:
+                    
+
+                    list = new MyArrayList<int>(10);
+                    linkedlist = new MyLinkedList<int>();
+                    for (size = 100; size <= 1000; size *= 10)
+                    {
+                        double sum = 0;
+                        double sum1 = 0;
+                        for (int j = 0; j < 20; j++)
+                        {
+                            for (int i = 0; i < size; i++) list.Add(i);
+                            for (int i = 0; i < size; i++) linkedlist.Add(i);
+
+                            Random random = new Random();
+                            Stopwatch stopwatch = new Stopwatch();
+                            stopwatch.Start();
+                            for (int i = 0; i < size; i++)
+                            {
+                                int index = random.Next(0, list.Size() - 1);
+                                int number = random.Next(0, 100000); list.Set(index, number);
+                            }
+                            stopwatch.Stop();
+                            sum += stopwatch.ElapsedMilliseconds;
+
+                            Stopwatch stopwatch1 = new Stopwatch();
+                            stopwatch1.Start();
+                            for (int i = 0; i < size; i++) {
+                                int index = random.Next(0, linkedlist.Size() - 1);
+                                int number = random.Next(0, 100000); linkedlist.Set(index, number);
+                            }
+                            stopwatch1.Stop();
+                            sum1 += stopwatch1.ElapsedMilliseconds;
+                        }
+                        double rez = sum / 20;
+                        double rez2 = sum1 / 20;
+                        list1.Add(size, rez);
+                        list2.Add(size, rez2);
+                        list.Clear();
+                        linkedlist.Clear();
+                    }
+                    break;
+
+                   
+                case 4:
+                    list = new MyArrayList<int>(10);
+                    linkedlist = new MyLinkedList<int>();
+                    for (size = 100; size <= 10000; size *= 10)
+                    {
+                        double sum = 0;
+                        double sum1 = 0;
+                        for (int j = 0; j < 20; j++)
+                        {
+                            for (int i = 0; i < size; i++) list.Add(i);
+                            for (int i = 0; i < size; i++) linkedlist.Add(i);
+
+                            Random rand = new Random();
+                            Stopwatch stopwatch = new Stopwatch();
+                            stopwatch.Start();
+                            for (int i = 0; i < size; i++)
+                            {
+                                int index = rand.Next(0, list.Size() - 1); list.Remove(index);
+                            }
+                            stopwatch.Stop();
+                            sum += stopwatch.ElapsedMilliseconds;
+
+                            Stopwatch stopwatch1 = new Stopwatch();
+                            stopwatch1.Start();
+                            for (int i = 0; i < size; i++)
+                            {
+                                int index = rand.Next(0, linkedlist.Size()-1); linkedlist.Remove(index);
+                            }
+                            stopwatch1.Stop();
+                            sum1 += stopwatch1.ElapsedMilliseconds;
+                        }
+                        double rez = sum / 20;
+                        double rez2 = sum1 / 20;
+                        list1.Add(size, rez);
+                        list2.Add(size, rez2);
+                        list.Clear();
+                        linkedlist.Clear();
+                    }
+                    break;
+            }
+            pane.CurveList.Clear();
+            pane.AddCurve("MyArrayList", list1, Color.HotPink, SymbolType.None);
+            ((LineItem)pane.CurveList[0]).Line.Width = 2.0f;
+
+            pane.AddCurve("MyLinkedList", list2, Color.Purple, SymbolType.None);
+            ((LineItem)pane.CurveList[1]).Line.Width = 2.0f;
+
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
